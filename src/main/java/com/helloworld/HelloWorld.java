28955c0a59cfc86145ca22385db8038fdfa7402e
YannisCxy <Cxyyu23@163.com>
2018-08-03 16:59:06 +0800

    first commit

---
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.helloworld;
+
+import com.sun.org.apache.xml.internal.utils.StringBufferPool;
+
+/**
+ * @author Yannic
+ * @version ${VERSION}
+ * @description com.helloworld
+ * @date 2018/8/3
+ */
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+    }
+}
